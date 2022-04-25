78d51bb20371f53bb4aea38bcb630abbb03ce2e3
H <289096333@qq.com>
2022-04-25 10:43:15 +0800

    # delete vue2 vue-count-to package # add vue3 vue-count-to component # delete edit dashboard

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -1,56 +1,55 @@
 <template>
     <div class="dashboard-editor-container">
-<!--        <github-corner class="github-corner" />-->
-
-<!--        <panel-group @handleSetLineChartData="handleSetLineChartData" />-->
 
         <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
-<!--            <line-chart :chart-data="lineChartData" />-->
-            <h1>123</h1>
-            <el-button type="primary">
-                <el-icon style="vertical-align: middle;">
-                    <search />
-                </el-icon>
-                <span style="vertical-align: middle;"> Search </span>
-            </el-button>
+            <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
         </el-row>
 
         <el-row :gutter="32">
             <el-col :xs="24" :sm="24" :lg="8">
                 <div class="chart-wrapper">
-<!--                    <raddar-chart />-->
+                    <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
                 </div>
             </el-col>
             <el-col :xs="24" :sm="24" :lg="8">
                 <div class="chart-wrapper">
-<!--                    <pie-chart />-->
+                    <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
                 </div>
             </el-col>
             <el-col :xs="24" :sm="24" :lg="8">
                 <div class="chart-wrapper">
-<!--                    <bar-chart />-->
+                    <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
                 </div>
             </el-col>
         </el-row>
 
         <el-row :gutter="8">
             <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-<!--                <transaction-table />-->
+                <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
             </el-col>
             <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-<!--                <todo-list />-->
+                <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
             </el-col>
             <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-<!--                <box-card />-->
+                <count-to :startVal='startVal' :endVal='endVal' :duration='3000'></count-to>
             </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, defineAsyncComponent } from 'vue'
 export default defineComponent({
-    name: 'index'
+    name: 'index',
+    components: {
+        countTo: defineAsyncComponent(() => import('@/components/vueCountTo/vue-countTo'))
+    },
+    data(){
+        return {
+            startVal: 0,
+            endVal: 2000,
+        }
+    },
 })
 </script>
 
